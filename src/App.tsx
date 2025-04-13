14e06c89eabf0445d693ff4084d757e61270c97e
Devanshi <devanshi.mehta.0018@gmail.com>
2025-04-13 18:52:31 +0530

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Rishabh weds Devanshi!
+          {/* Edit <code>src/App.tsx</code> and save to reload. */}
         </p>
         <a
           className="App-link"
